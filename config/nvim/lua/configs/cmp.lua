762c267413211807e06a26068abdc5a7fb29f007
Tyler Ward <tyler.t.ward@icloud.com>
2021-10-15 10:04:25 -0500

    feat: add tab navigation for completions

--- config/nvim/lua/configs/cmp.lua
+++ config/nvim/lua/configs/cmp.lua
@@ -18,6 +18,8 @@ cmp.setup {
 		end,
 	},
 	mapping = {
+		['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select }),
+		['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select }),
 		['<C-d>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
@@ -26,24 +28,6 @@ cmp.setup {
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
 		},
-		['<Tab>'] = function(fallback)
-			if vim.fn.pumvisible() == 1 then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
-			elseif require('luasnip').expand_or_jumpable() then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
-			else
-				fallback()
-			end
-		end,
-		['<S-Tab>'] = function(fallback)
-			if vim.fn.pumvisible() == 1 then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
-			elseif require('luasnip').jumpable(-1) then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
-			else
-				fallback()
-			end
-		end,
 	},
 	sources = {
 		{ name = 'nvim_lsp' },
