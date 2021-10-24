6d38b708d1821274885f6f2bec5857b6adf841c2
Tyler Ward <tyler.t.ward@icloud.com>
2021-10-23 23:31:30 -0500

    Add Neorg

--- config/nvim/lua/configs/treesitter.lua
+++ config/nvim/lua/configs/treesitter.lua
@@ -2,6 +2,14 @@ local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
 
 parser_configs.tsx.used_by = { 'javascript', 'typescript.tsx' }
 
+parser_configs.norg = {
+	install_info = {
+		url = 'https://github.com/nvim-neorg/tree-sitter-norg',
+		files = { 'src/parser.c', 'src/scanner.cc' },
+		branch = 'main',
+	},
+}
+
 require('nvim-treesitter.configs').setup {
 	highlight = {
 		enable = true,
