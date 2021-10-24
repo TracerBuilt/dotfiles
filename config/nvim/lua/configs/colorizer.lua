08bb6072b48c079491a4063fc1df25ffe1fa5724
Tyler Ward <tyler.t.ward@icloud.com>
2021-10-23 23:30:05 -0500

    Finally get colorizer to work (probably)

--- config/nvim/lua/configs/colorizer.lua
+++ config/nvim/lua/configs/colorizer.lua
@@ -3,10 +3,11 @@ require('colorizer').setup({
 	css = { names = true },
 	scss = { names = true },
 }, {
-	mode = 'background',
-	css_fn = true,
 	names = false,
 	RGB = true,
 	RRGGBB = true,
 	RRGGBBAA = true,
+	rgb_fn = true,
+	hsl_fn = true,
+	mode = 'background',
 })
