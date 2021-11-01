bc0dfbe30afa26a6843b540207cf5b669093d775
Tyler Ward <tyler.t.ward@icloud.com>
2021-11-01 16:12:03 -0500

    Revamp cmp configuration

--- config/nvim/lua/configs/cmp.lua
+++ config/nvim/lua/configs/cmp.lua
@@ -19,22 +19,40 @@ cmp.setup {
 		end,
 	},
 	mapping = {
-		['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select }),
-		['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select }),
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.close(),
-		['<CR>'] = cmp.mapping.confirm {
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
+		['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+		['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+		['<Down>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+		['<Up>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+		['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+		['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+		['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+		['<C-y>'] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
+		['<C-e>'] = cmp.mapping {
+			i = cmp.mapping.abort(),
+			c = cmp.mapping.close(),
 		},
+		['<CR>'] = cmp.mapping.confirm { select = true },
 	},
 	sources = {
 		{ name = 'neorg' },
-		{ name = 'cmp_tabnine' },
 		{ name = 'nvim_lsp' },
 		{ name = 'luasnip' },
 		{ name = 'buffer' },
 	},
 }
+
+-- Use buffer source for `/`.
+cmp.setup.cmdline('/', {
+	sources = {
+		{ name = 'buffer' },
+	},
+})
+
+-- Use cmdline & path source for ':'.
+cmp.setup.cmdline(':', {
+	sources = cmp.config.sources({
+		{ name = 'path' },
+	}, {
+		{ name = 'cmdline' },
+	}),
+})
