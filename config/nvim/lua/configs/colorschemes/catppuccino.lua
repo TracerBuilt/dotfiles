c6430c01046aa2c61b2503b3a93ed94c0042823b
Tyler Ward <tyler.t.ward@icloud.com>
2021-11-01 16:30:51 -0500

    Add color scheme configs

---
+++ config/nvim/lua/configs/colorschemes/catppuccino.lua
@@ -0,0 +1,74 @@
+local catppuccino = require 'catppuccino'
+
+catppuccino.setup {
+	colorscheme = 'dark_catppuccino',
+	transparency = true,
+	term_colors = true,
+	styles = {
+		comments = 'italic',
+		functions = 'italic',
+		keywords = 'italic',
+		strings = 'NONE',
+		variables = 'NONE',
+	},
+	integrations = {
+		treesitter = true,
+		native_lsp = {
+			enabled = true,
+			virtual_text = {
+				errors = 'italic',
+				hints = 'italic',
+				warnings = 'italic',
+				information = 'italic',
+			},
+			underlines = {
+				errors = 'undercurl',
+				hints = 'undercurl',
+				warnings = 'undercurl',
+				information = 'undercurl',
+			},
+		},
+		lsp_trouble = true,
+		lsp_saga = false,
+		gitgutter = false,
+		gitsigns = true,
+		telescope = true,
+		nvimtree = {
+			enabled = true,
+			show_root = true,
+		},
+		which_key = true,
+		indent_blankline = {
+			enabled = true,
+			colored_indent_levels = true,
+		},
+		dashboard = false,
+		neogit = false,
+		vim_sneak = false,
+		fern = false,
+		barbar = true,
+		bufferline = false,
+		markdown = true,
+		lightspeed = true,
+		ts_rainbow = true,
+		hop = false,
+	},
+}
+
+vim.cmd 'colorscheme catppuccino'
+
+local cp_api = require 'catppuccino.api.colors'
+local _, colors = cp_api.get_colors 'dark_catppuccino'
+vim.api.nvim_set_var(
+	'colors',
+	{ error = colors.error, warning = colors.warning, info = colors.info, hint = colors.hint, white = colors.white }
+)
+
+vim.cmd [[
+	hi DiagnosticUnderline gui=undercurl
+	hi DiagnosticUnderlineError gui=undercurl 
+	hi DiagnosticUnderlineWarn gui=undercurl 
+	hi DiagnosticUnderlineInfo gui=undercurl 
+	hi DiagnosticUnderlineHint gui=undercurl
+	hi DiagnosticUnderlineSuggestion gui=undercurl
+	]]
---
+++ config/nvim/lua/configs/colorschemes/tokyonight.lua
@@ -0,0 +1,6 @@
+vim.g.tokyonight_style = 'night'
+vim.g.tokyonight_transparent = true
+vim.g.tokyonight_sidebars = { 'terminal', 'packer', 'NvimTree' }
+vim.g.tokyonight_transparent_sidebar = true
+
+-- vim.cmd 'colorscheme tokyonight'
