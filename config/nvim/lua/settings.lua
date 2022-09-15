c6f108268ef335d0847d0de699a01e322b3cc283
Tyler Ward <tyler.t.ward@icloud.com>
2022-09-15 11:22:44 -0500

    add raycast

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -36,6 +36,8 @@ vim.o.shiftwidth = 2
 vim.o.smartindent = true
 vim.o.breakindent = true
 
+vim.g.neovide_transparency = 0.8
+
 --Remap for dealing with word wrap
 vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
 vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
