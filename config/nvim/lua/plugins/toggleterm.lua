044e3e09c69b5a07b845752e58a4ae27f499a3a6
TracerBuilt <tyler@tracerbuilt.dev>
2024-05-28 12:02:27 -0500

    refactor toggleterm config

--- config/nvim/lua/plugins/toggleterm.lua
+++ config/nvim/lua/plugins/toggleterm.lua
@@ -9,25 +9,19 @@ return {
 			shade_filetypes = {},
 			start_in_insert = true,
 			insert_mappings = true, -- whether or not the open mapping applies in insert mode
+			on_open = function(term)
+				local opts = { buffer = term.bufnr }
+				vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+				vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+				vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+				vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+				vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+				vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+				vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+			end,
 			persist_size = true,
 			direction = 'horizontal',
 		}
-
-		local wk = require 'which-key'
-
-		function _G.set_terminal_keymaps()
-			local opts = { buffer = 0 }
-			vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-			vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-			vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-			vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-			vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-			vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-			vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-		end
-
-		-- if you only want these mappings for toggle term use term://*toggleterm#* instead
-		vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
 
 		local Terminal = require('toggleterm.terminal').Terminal
 		local lazygit = Terminal:new {
@@ -35,9 +29,9 @@ return {
 			dir = 'git_dir',
 			hidden = true,
 			direction = 'float',
-			on_open = function(term)
+			on_open = function()
 				vim.cmd 'startinsert!'
-				vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
+				vim.keymap.set('n', 'q', '<cmd>close<CR>', { noremap = true, silent = true, buffer = 0 })
 			end,
 			on_close = function(term)
 				vim.cmd 'startinsert!'
@@ -46,6 +40,8 @@ return {
 		function _lazygit_toggle()
 			lazygit:toggle()
 		end
+
+		local wk = require 'which-key'
 
 		wk.register({
 			['<leader>g'] = {
