d10f276d9b550149c810c29379b12d712c01d3bf
Tyler <tyler@tracerbuilt.dev>
2024-07-07 09:47:28 -0500

    remove telescope borders

---
+++ config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,16 @@
+return {
+	'nvim-telescope/telescope.nvim',
+	opts = {
+		defaults = {
+			border = false,
+		},
+		resolve = {
+			resolve_height = {
+				padding = 1,
+			},
+			resolve_width = {
+				padding = 1,
+			},
+		},
+	},
+}
