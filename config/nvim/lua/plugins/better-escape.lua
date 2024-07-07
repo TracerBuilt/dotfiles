6e7faa9ff89b0207395f99949bed3a5650a35c0d
Tyler <tyler@tracerbuilt.dev>
2024-07-07 09:48:20 -0500

    add better-escape

---
+++ config/nvim/lua/plugins/better-escape.lua
@@ -0,0 +1,33 @@
+return {
+	'max397574/better-escape.nvim',
+	opts = {
+		mappings = {
+			i = {
+				j = {
+					-- These can all also be functions
+					k = '<Esc>',
+				},
+			},
+			c = {
+				j = {
+					k = '<Esc>',
+				},
+			},
+			t = {
+				j = {
+					k = '<Esc>',
+				},
+			},
+			v = {
+				j = {
+					k = '<Esc>',
+				},
+			},
+			s = {
+				j = {
+					k = '<Esc>',
+				},
+			},
+		},
+	},
+}
