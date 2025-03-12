494af89c2401408cebf5c4b67243e1c162a438cb
Tyler Ward <tyler@tracerbuilt.dev>
2025-03-12 10:52:54 -0500

    add Zed config

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,7 @@
 # You can build them using 'nix build .#example'
 pkgs: {
   # example = pkgs.callPackage ./example { };
-  cider-2 = pkgs.callPackage ./cider {};
+  cider = pkgs.callPackage ./cider {};
   workflowy = pkgs.callPackage ./workflowy {};
+  noi = pkgs.callPackage ./noi {};
 }
